64274ecd53a830dbc032e03bbc1d78b164ac18d8
thiagorabaioli <tfrabaioli@outlook.com>
2024-01-27 18:10:33 +0000

    first commit

---
+++ backend/src/main/java/tfr/APPHOME/AppHomeApplication.java
@@ -0,0 +1,13 @@
+package tfr.APPHOME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHomeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tfr/APPHOME/AppHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package tfr.APPHOME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
