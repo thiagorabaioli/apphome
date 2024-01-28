7d6247b4caad771c372b004e5d2e28cd46b62418
thiagorabaioli <tfrabaioli@outlook.com>
2024-01-28 21:04:52 +0000

    OccurrenceService

--- backend/src/main/java/tfr/APPHOME/services/OccurrenceService.java
+++ backend/src/main/java/tfr/APPHOME/services/OccurrenceService.java
@@ -1,7 +1,11 @@
 package tfr.APPHOME.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import tfr.APPHOME.dto.OccurrenceDTO;
+import tfr.APPHOME.entities.Occurrence;
 import tfr.APPHOME.repositories.OccurrenceRepository;
 
 @Service
@@ -10,5 +14,9 @@ public class OccurrenceService {
     @Autowired
     private OccurrenceRepository repo;
 
+    public Page<OccurrenceDTO> findAll(Pageable pageable){
+        Page<Occurrence> entity = repo.findAll(pageable);
+        return entity.map(x -> new OccurrenceDTO(x));
+    }
 
 }
