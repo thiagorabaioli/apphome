6bec864ec1a713156f92765640697a1913dc02d1
thiagorabaioli <tfrabaioli@outlook.com>
2024-01-28 19:49:10 +0000

    complet Java Class UserAPP and Occurrence

--- backend/src/main/java/tfr/APPHOME/entities/Occurrence.java
+++ backend/src/main/java/tfr/APPHOME/entities/Occurrence.java
@@ -19,7 +19,85 @@ public class Occurrence {
     private PRIORITY priority;
 
     private String img;
-    
 
+    private UserAPP userAPP;
 
+    public Occurrence(){}
+
+    public Occurrence(Long id, String occorrenceName, Date dataOpenOccorrence,
+                      Date dataCloseOccorrence, STATEOFOCCURRENCE state, PRIORITY priority, String img, UserAPP userAPP) {
+
+        this.id = id;
+        this.occorrenceName = occorrenceName;
+        this.dataOpenOccorrence = dataOpenOccorrence;
+        this.dataCloseOccorrence = dataCloseOccorrence;
+        this.state = state;
+        this.priority = priority;
+        this.img = img;
+        this.userAPP = userAPP;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOccorrenceName() {
+        return occorrenceName;
+    }
+
+    public void setOccorrenceName(String occorrenceName) {
+        this.occorrenceName = occorrenceName;
+    }
+
+    public Date getDataOpenOccorrence() {
+        return dataOpenOccorrence;
+    }
+
+    public void setDataOpenOccorrence(Date dataOpenOccorrence) {
+        this.dataOpenOccorrence = dataOpenOccorrence;
+    }
+
+    public Date getDataCloseOccorrence() {
+        return dataCloseOccorrence;
+    }
+
+    public void setDataCloseOccorrence(Date dataCloseOccorrence) {
+        this.dataCloseOccorrence = dataCloseOccorrence;
+    }
+
+    public STATEOFOCCURRENCE getState() {
+        return state;
+    }
+
+    public void setState(STATEOFOCCURRENCE state) {
+        this.state = state;
+    }
+
+    public PRIORITY getPriority() {
+        return priority;
+    }
+
+    public void setPriority(PRIORITY priority) {
+        this.priority = priority;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public UserAPP getUserAPP() {
+        return userAPP;
+    }
+
+    public void setUserAPP(UserAPP userAPP) {
+        this.userAPP = userAPP;
+    }
 }
--- backend/src/main/java/tfr/APPHOME/entities/UserAPP.java
+++ backend/src/main/java/tfr/APPHOME/entities/UserAPP.java
@@ -1,12 +1,55 @@
 package tfr.APPHOME.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+
+
 public class UserAPP {
 
     private Long id;
     private String name;
     private String email;
 
+    private Set<Occurrence> occurrences = new HashSet<>();
 
+    public UserAPP(){}
 
+    public UserAPP(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Occurrence> getOccurrences() {
+        return occurrences;
+    }
+
+    public void setOccurrences(Set<Occurrence> occurrences) {
+        this.occurrences = occurrences;
+    }
 }
